0d096d73ae1572758d8caf7fbb84d54f90d268fc
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2024-12-04 18:17:26 +0000

    implement custom volume feature extractor

---
+++ network.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+from gymnasium import spaces
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+
+
+class Custom3DCNN(BaseFeaturesExtractor):
+
+    def __init__(self, observation_space: spaces.Box, features_dim: int = 256):
+        super().__init__(observation_space, features_dim)
+        n_input_channels = observation_space.shape[0]
+        self.cnn = nn.Sequential(
+            nn.Conv3d(n_input_channels, 32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv3d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv3d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        with torch.no_grad():
+            sample_input = torch.as_tensor(observation_space.sample()[None]).float()
+            n_flatten = self.cnn(sample_input).shape[1]
+
+        self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+
+    def forward(self, observations: torch.Tensor) -> torch.Tensor:
+        return self.linear(self.cnn(observations))
+
+
+def test_custom_cnn():
+    from environment import RadiotherapyEnv
+
+    env = RadiotherapyEnv()
+
+    policy_kwargs = dict(
+        features_extractor_class=Custom3DCNN,
+        features_extractor_kwargs=dict(features_dim=128),
+    )
+
+    model = PPO("CnnPolicy", env, policy_kwargs=policy_kwargs, verbose=1)
+
+    model.learn(10)
+
+
+if __name__ == "__main__":
+    test_custom_cnn()
