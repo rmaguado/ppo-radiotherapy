fix beam scaling

environment.py
@@ -25,7 +25,8 @@ class RadiotherapyEnv(gym.Env):
 
     TUMOUR_DIRS = [x for x in os.listdir("./data/tumours") if x.endswith(".npy")]
     LUNGS_ARRAY = np.load("./data/lungs.npy").astype(np.float32)
-    LUNG_SHAPE = LUNGS_ARRAY.shape
+    LUNG_SHAPE = np.array(LUNGS_ARRAY.shape)
+    TRANSLATION_BOUNDS = LUNG_SHAPE - 1
     OBSERVATION_SHAPE = (LUNG_SHAPE[0], LUNG_SHAPE[1], LUNG_SHAPE[2], 4)
 
     metadata = {"render_modes": ["human"], "render_fps": 30}
@@ -80,11 +81,16 @@ class RadiotherapyEnv(gym.Env):
 
     def reset_beam(self):
         self.beams = []
-        self.beam_position = np.array([0.0, 0.0, 0.0], dtype=np.float32)
+        self.beam_position = np.array(
+            [0.0, 0.0, 0.0], dtype=np.float32
+        )  # corner of the lung box
         self.beam_direction = np.array([0.0, 0.0, 1.0], dtype=np.float32)
 
     def reset_dose(self):
         self.dose = np.zeros_like(self.lungs, dtype=np.float32)
+
+    def grid_to_world(self, grid_position):
+        box_shape = np.array(self.LUNG_SHAPE)
 
     def add_beam(self, position, direction):
         self.dose += beam_voxels(self.lungs, position, direction)
@@ -101,9 +107,7 @@ class RadiotherapyEnv(gym.Env):
             np.ndarray: The translation vector. Range is [0, shape - 1].
         """
 
-        translation_bounds = np.array(self.LUNG_SHAPE) - 1
-
-        return normalized_translation * translation_bounds
+        return normalized_translation * self.TRANSLATION_BOUNDS
 
     def map_rotation(self, normalized_rotation):
         """
@@ -166,7 +170,7 @@ class RadiotherapyEnv(gym.Env):
         rotation = self.map_rotation(normalized_rotation)
 
         new_position, overshoot_translation = apply_translation(
-            self.beam_position, translation, np.array(self.LUNG_SHAPE) - 1
+            self.beam_position, translation, self.TRANSLATION_BOUNDS
         )
         new_direction, overshoot_rotation = apply_rotation(
             self.beam_direction, rotation, self.MAX_SLOPE
@@ -197,9 +201,10 @@ class RadiotherapyEnv(gym.Env):
         create_animation(
             self.tumours_meta,
             self.beams,
-            f"animations/{timestamp}.gif",
-            self.export_gif,
-            True,
+            self.LUNG_SHAPE,
+            filename=f"animations/{timestamp}.gif",
+            export_gif=self.export_gif,
+            window=True,
         )
 
     def inspect_observation(self):
@@ -224,10 +229,18 @@ def test_observation_render():
 
     env.inspect_observation()
 
-    # add default beam
     env.step(np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0]))
 
     env.inspect_observation()
+
+    env.step(np.array([0.1, 0.0, 0.0, 0.5, 0.0, 0.0, 1.0, 0.0]))
+
+    env.inspect_observation()
+
+    env.step(np.array([0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0]))
+
+    env.inspect_observation()
+
     env.render()
     env.close()
 

graphics.py
@@ -81,10 +81,15 @@ def create_beam(
     direction: np.ndarray,
     radius: float = 0.1,
     length: float = 10.0,
+    use_center: bool = True,
 ) -> trimesh.Trimesh:
     beam = trimesh.creation.cylinder(radius=radius, height=length, sections=8)
 
-    lungs_position = lungs_mesh.bounding_box.centroid
+    if use_center:
+        lungs_position = lungs_mesh.bounding_box.centroid
+    else:
+        # corner
+        lungs_position = lungs_mesh.bounds[0]
     translation = position + lungs_position
 
     direction = direction / np.linalg.norm(direction)
@@ -329,6 +334,7 @@ def export_scene(scene, resolution=(800, 600)):
 def create_animation(
     tumours_data,
     beams_data,
+    lung_shape=None,
     filename="animations/test.gif",
     export_gif=True,
     window=False,
@@ -336,9 +342,18 @@ def create_animation(
     human_model = load_human_model()
     lungs = load_lungs_model()
 
+    lungs_bounds = lungs.bounds
+    if lung_shape is not None:
+        beam_scaling = (lungs_bounds[1] - lungs_bounds[0]) / np.array(lung_shape)
+    else:
+        beam_scaling = 1.0
+
     tumours = [get_tumour(position, radius) for (position, radius) in tumours_data]
     beams = [
-        create_beam(lungs, position, direction) for (position, direction) in beams_data
+        create_beam(
+            lungs, position * beam_scaling, direction * beam_scaling, use_center=False
+        )
+        for (position, direction) in beams_data
     ]
 
     camera_transform = np.load("camera_transform.npy")

transforms.py
@@ -60,7 +60,7 @@ def apply_translation(
     position: np.ndarray, translation_vector: np.ndarray, bounds: np.ndarray
 ) -> Tuple[np.ndarray, np.ndarray]:
     translated_position = position + translation_vector
-    bounded_new_position = np.clip(translated_position, bounds[0], bounds[1])
+    bounded_new_position = np.clip(translated_position, 0, bounds)
     overshoot = np.abs(translated_position - bounded_new_position)
 
     return bounded_new_position, overshoot